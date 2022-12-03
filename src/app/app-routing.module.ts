8074a7f4ce270d464d334785f844288d4bfb9a12
Kapee87 <kapserker@gmail.com>
2022-12-03 12:25:01 -0300

    commit previo a agregar bootstrap para angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,18 +5,19 @@ import { BannerComponent } from './components/banner/banner.component';
 import { IntroComponent } from './components/intro/intro.component';
 import { LoginComponent } from './components/login/login.component';
 import { NotFound404Component } from './components/not-found404/not-found404.component';
+import { SobreMiComponent } from './components/sobre-mi/sobre-mi.component';
 
 const routes: Routes = [
-  {path:'', redirectTo:'/inicio',pathMatch:'full'},
-  {path: 'inicio', component: BannerComponent},
-  {path: 'introduccion', component: IntroComponent},
-  {path:'login', component: LoginComponent},
-  {path:'**', component:NotFound404Component}
-  
+  { path: '', redirectTo: '/inicio', pathMatch: 'full' },
+  { path: 'inicio', component: BannerComponent },
+  { path: 'introduccion', component: IntroComponent },
+  { path: 'sobre-mi', component: SobreMiComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '**', component: NotFound404Component },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
