6b1f8285a0e57f3253b5fabeda7cccea5e4c29cc
unknown <kapee87>
2023-01-06 15:19:14 -0300

    web previa a inicializar firebase

--- src/app/pages/sobre-mi/sobre-mi.component.ts
+++ src/app/pages/sobre-mi/sobre-mi.component.ts
@@ -5,39 +5,41 @@ import { PersonaService } from 'src/app/service/persona.service';
 @Component({
   selector: 'app-sobre-mi',
   templateUrl: './sobre-mi.component.html',
-  styleUrls: ['./sobre-mi.component.css']
+  styleUrls: ['./sobre-mi.component.css'],
 })
 export class SobreMiComponent implements OnInit {
   varclass = true;
-  persona: Persona = new Persona("", "", "", "", "", "", "");
-  // personasList: Persona[] | undefined;
+  persona: Persona = new Persona('', '', '', '', '', '', '');
+  personasList: Persona[] | undefined;
+  personaId: number | undefined;
   telefonoString: String | undefined;
   correoString: String | undefined;
 
-  constructor(public personaService: PersonaService) { }
+  constructor(public personaService: PersonaService) {}
 
-   ngOnInit(): void {
-    this.personaService.getPersona().subscribe(data=> {
-      this.persona = data;
+  ngOnInit(): void {
+    this.personaService.getPersonas().subscribe((data) => {
+      this.persona = data[0];
       this.telefonoString = `https://wa.me/2257665721`;
-      this.correoString= "mailto:"+ data.correo;
-      console.log(data)
-
-       if (data.id !== 0 || data.id !== null || data.id !== undefined){
+      this.correoString = 'mailto:' + data[0].correo;
+      if (data[0].id !== 0 || data[0].id !== null || data[0].id !== undefined) {
         setTimeout(() => {
           this.varclass = !this.varclass;
-        }, 500)};
+        }, 500);
+      }
+    });
 
-      });
+    // this.personaService.getPersona().subscribe(data=> {
+    //   this.persona = data;
 
+    //   console.log(data)
 
+    //   });
 
-      //para request que devuelven listas.
-      // this.personaService.getPersonas().subscribe(personas=>{
-      //   this.personasList = personas
-      //   console.log(this.personasList[0]);
-      // })
+    //para request que devuelven listas.
+    // this.personaService.getPersonas().subscribe(personas=>{
+    //   this.personasList = personas
+    //   console.log(this.personasList[0]);
+    // })
   }
-
-  
 }
